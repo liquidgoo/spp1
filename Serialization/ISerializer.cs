7b80bf493124568a5a01c4877d55c1fc6204fde0
liquidgoo <60588318+liquidgoo@users.noreply.github.com>
2021-10-13 07:30:47 +0300

    Add files via upload

---
+++ Serialization/ISerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracer;
+
+namespace Serialization
+{
+    public interface ISerializer
+    {
+        public void Serialize(TraceResult result);
+
+        public 
+       
+    }
+}
---
+++ Serialization/JsonSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using Tracer;
+
+namespace Serialization
+{
+    public class JsonSerializer : ISerializer
+    {
+        public void Serialize(Stream outputStream, TraceResult result)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            byte[] bytes = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(result, options);
+            outputStream.Write(bytes);
+        }
+    }
+}
